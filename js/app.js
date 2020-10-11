1aaa1f9781eb972803ae3aa91b5585ec25e760d2
Bazina <60438793+Bazina@users.noreply.github.com>
2020-10-11 19:42:48 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -13,45 +13,67 @@
  * 
 */
 
-/**
- * Define Global Variables
- * 
-*/
+/* Define Global Variables */
+
+const navBar = document.getElementById("navbar__list");
+const secElements = document.querySelectorAll("section");
+
+/* End Global Variables */
+
+/* build the nav, Build menu */
+
+secElements.forEach(sec => {
+    const navBarElement = `<li class="menu__link" data-link="${sec.id}" ><a href = "#${sec.id}">${sec.dataset.nav}</a></li> `;
+    navBar.insertAdjacentHTML("beforeend",navBarElement);
+});
+
+/* Scroll to anchor ID using scrollTO event, Scroll to section on link click */
+
+navBar.addEventListener("click", evt =>{
+    evt.preventDefault();
+    const ancestor = evt.target.hasAttribute("data-link") ? evt.target : evt.target.parentElement;
+    const elScroll = document.getElementById(ancestor.dataset.link);
+    let firstX = window.scrollX;
+    let firstY = window.scrollY;
+    let nowX = elScroll.getBoundingClientRect().x;
+    let nowY = elScroll.getBoundingClientRect().y;
+    let goX = nowX + firstX;
+    let goY = nowY + firstY;
+    window.scrollTo({
+        left : goX,
+        top : goY,
+        behavior : "smooth"
+    });
+});
+
+/* Add class 'active' to section when near top of viewport, Set sections as active */
 
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        const navBarElement = document.querySelector(
+        `.menu__link[data-link='${entry.target.id}']`
+        );
+        const secActive = document.getElementById(entry.target.id);
+        if (entry.isIntersecting) {
+        navBarElement.classList.add('active')
+        secActive.classList.add('active')
+        } else {
+        if (navBarElement.classList.contains('active')) {
+            navBarElement.classList.remove('active')
+        }
+        if (secActive.classList.contains('active')) {
+            secActive.classList.remove('active')
+        }
+        }
+    })
+},
+    {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.6,
+    }
+    )
+    secElements.forEach(el => {
+        observer.observe(document.getElementById(el.id))
+    })
